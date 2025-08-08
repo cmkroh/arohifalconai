e7d1b9a6fb651c04670e35c50f11c987af964aba
cmkroh <cmkroh@gmail.com>
2025-08-09 01:11:00 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a basePath if you're deploying to a subpath
+  // basePath: '/my-next-app',
+};
+
+module.exports = nextConfig;
