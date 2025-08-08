290ab3508257a43a5e2ef2081a75ee9fe467fe5b
cmkroh <cmkroh@gmail.com>
2025-08-09 01:15:28 +0600

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // Optional: Add a basePath if you're deploying to a subpath
-  // basePath: '/my-next-app',
-};
-
-module.exports = nextConfig;
